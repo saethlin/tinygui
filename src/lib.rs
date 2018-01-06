7e4c872b1d529adc13cd71e1c1c7288548ba12ec
Ben Kimock <kimockb@gmail.com>
2018-01-05 21:27:28 -0500

    adding App struct

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,25 @@ mod display;
 pub use widget::Widget;
 pub use window::{Event, Window};
 pub use display::Display;
+
+#[derive(Default)]
+pub struct App {
+    window: Window,
+    widgets: Vec<Box<Widget>>,
+}
+
+impl App {
+    pub fn run(&mut self) -> {
+        self.window.map_and_flush();
+        for event in window.events() {
+            for widget in self.widgets {
+                widget.handle(event);
+            }
+        }
+    }
+
+    pub fn close(&mut self) -> {
+        self.should_close = true;
+    }
+}
+
